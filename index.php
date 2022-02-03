4954324bc70868a4221c20fb1d0b7f0a24dde786
LAPTOP-6PDJSNIR\Dimasf_ <Dimasfebriansyah231@gmail.com>
2022-02-03 11:42:39 +0700

    menambahkan data index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git Remote</title>
+</head>
+<body>
+    <h1>hello world</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo ipsa sed itaque veniam consectetur facere esse tempore! Repudiandae libero velit pariatur, veniam ab a ipsam blanditiis necessitatibus accusamus? Suscipit, est.</p>
+</body>
+</html>
